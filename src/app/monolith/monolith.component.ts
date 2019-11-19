912a83a8a089d128c948386b06290fbbadf1503d
Jeff <Haske107@mail.chapman.edu>
2019-11-19 14:35:32 -0800

    s

--- src/app/monolith/monolith.component.ts
+++ src/app/monolith/monolith.component.ts
@@ -12,13 +12,51 @@ export class MonolithComponent implements OnInit, AfterViewInit {
 
   @Input() BannerVideoScaleValue;
   @Input() BannerRotateValue;
+  Player1;
 
-
+  options = {
+    id: 322980163,
+    muted: true,
+    background: true,
+    loop: true,
+  };
 
   ngOnInit() {
   }
 
   ngAfterViewInit() {
+    const playerContainer = document.getElementById('iframe59218');
+    this.Player1 = new Player(playerContainer, this.options);
+
+    window.setTimeout(this.playIfPaused(), 3000);
+
+  }
+
+  playIfPaused()  {
+    this.Player1.getPaused().then((Paused) => {
+      if (Paused)  {
+        this.Player1.play().then(() => {
+          // the video was played
+          console.log("Force played");
+        }).catch((error) => {
+          switch (error.name) {
+            case 'PasswordError':
+              // the video is password-protected and the viewer needs to enter the
+              // password first
+              break;
+            case 'PrivacyError':
+              // the video is private
+              break;
+            default:
+              // some other error occurred
+              break;
+          }
+        });
+      }
+      // paused = whether or not the player is paused
+    }).catch((error) => {
+      // an error occurred
+    });
   }
 
 }
