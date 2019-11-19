acce8dba6dcad8fce20cde79bf5a25375efb27d9
Trikon Computer <trikoncomputer@Trikons-iMac.lan1>
2019-11-18 21:50:14 -0800

    s

--- src/app/monolith/monolith.component.ts
+++ src/app/monolith/monolith.component.ts
@@ -1,22 +1,24 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
+import Player from '@vimeo/player';
 
 @Component({
   selector: 'app-monolith',
   templateUrl: './monolith.component.html',
   styleUrls: ['./monolith.component.css']
 })
-export class MonolithComponent implements OnInit {
+export class MonolithComponent implements OnInit, AfterViewInit {
 
   constructor() { }
-
 
   @Input() BannerVideoScaleValue;
   @Input() BannerRotateValue;
 
-  Video = document.getElementById("iframe59218");
+
 
   ngOnInit() {
+  }
 
+  ngAfterViewInit() {
   }
 
 }
